a76449d5c9847b2b0d0bc7be64fb9072e568796c
AlaStepien <alicja-stepien@hotmail.com>
2021-12-18 14:26:21 +0100

    added improvements

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ guzik.addEventListener("click", showTable)
 function showTable() {
 alert("jutro")
 }
+let button = document.getElementById("button1")
+let count = 0 
+function increment() {
+  count = count + 1 
+  button1.innerText = count
+}
+
+function show(anything) {
+  document.querySelector('.textBox').value = anything;
+}
+
+let dropdown = document.querySelector('.dropdown');
+dropdown.onclick = function() {
+   dropdown.classList.toggle('active');
+}
